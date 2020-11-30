e2d0517d8ea24859c486697dd5e6633bffa76c69
ericsantos <eric@exactaworks.com.br>
2020-11-30 09:07:26 -0300

    chore: update dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,18 @@
 module.exports = {
-  'stories': [
+  stories: [
     '../src/components/**/stories.mdx',
     '../src/components/**/stories.@(js|jsx|ts|tsx)',
   ],
-  'addons': [
+  addons: [
     '@storybook/addon-essentials',
   ],
+  babel: async (options) => ({
+    ...options,
+    plugins: [...options.plugins, '@babel/plugin-transform-react-jsx'],
+  }),
+  webpackFinal: (config) => {
+    config.resolve.modules.push(`${process.cwd()}/src`);
+
+    return config;
+  }
 };
